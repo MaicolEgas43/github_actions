167cf7ad42a0ca3ecc43c289098fbfb1f485d5b7
Valentina <vmoncadam@unal.edu.co>
2025-05-07 20:32:10 -0500

    test pipeline

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#Valentina Moncada Morales
+#Maicol Estiben Egas
 import Estudiantes.registro as registro
+
 def main():
     # Cargar estudiantes desde el archivo CSV
     print("Cargando datos de estudiantes...")
     dataEstudiantes = registro.cargar_estudiantes("estudiantes_notas.csv")
     print(f'Notas estudiantes cargadas: {len(dataEstudiantes)}')
     
-    # Mostrar tabla de estudiantes ordenados alfabéticamente
+    # Mostrar tabla de estudiantes ordenados alfabéticamentes
     registro.imprimir_estudiantes_ordenados(dataEstudiantes)
     
     # Calcular y mostrar el promedio de notas
