1b6c94c6e29ac394262346409a66fe2eb0470281
Valentina <vmoncadam@unal.edu.co>
2025-05-07 20:13:02 -0500

    Integrar flujo completo de ejecución en main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    # Cargar estudiantes desde el archivo CSV
+    print("Cargando datos de estudiantes...")
+    dataEstudiantes = registro.cargar_estudiantes("estudiantes_notas.csv")
+    print(f'Notas estudiantes cargadas: {len(dataEstudiantes)}')
+    
+    # Mostrar tabla de estudiantes ordenados alfabéticamente
+    registro.imprimir_estudiantes_ordenados(dataEstudiantes)
+    
+    # Calcular y mostrar el promedio de notas
+    registro.calcular_promedio_notas(dataEstudiantes)
+
+if __name__ == "__main__":
+    main()
+    
